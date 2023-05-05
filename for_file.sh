8d26ec77c0dda914bb8556f9c15edf1772b77827
gmateu <gmateu@esliceu.com>
2023-05-05 07:03:58 +0200

    get vaules in variables

--- for_file.sh
+++ for_file.sh
@@ -2,6 +2,9 @@
 
 for line in $(cat /etc/passwd)
 do
+    echo $line | awk -F: '$3>=0 {print $1 , $3}'
     USER=$(echo $line | awk -F: '$3>=0 {print $1}')
+    NUM=$(echo $line | awk -F: '$3>=0 {print $3}')
     echo "user: $USER"
+    echo "UID: $NUM"
 done
